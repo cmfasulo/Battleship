cc06474eab1218ddec4761e47f78ebc2ad0a3a42
Toma, Eric <TomaandEric@LEARNAcademy.edu>
2016-12-06 13:58:16 -0800

    added spacing check for ship placements

--- battleshipModel.js
+++ battleshipModel.js
@@ -18,7 +18,7 @@ var torpedoCount = 25;
 
 var rows = 10;
 var columns = 10;
-var board = createBoard(rows, columns, "O") //createBoard(rows, columns, fillCharacter)
+var board = createBoard(rows, columns, "O"); //createBoard(rows, columns, fillCharacter)
 
 //creates a function to be called when a user clicks a square
 function playerClick() {
@@ -63,19 +63,39 @@ function addShip(ship, shipLength) {
 
         var endRow = rowStart + shipLength;
         var endColumn = columnStart + shipLength;
-        var backRow = rowStart - 1;
-        var forwardRow = rowStart + 1;
-        var backColumn = columnStart - 1;
-        var forwardColumn = columnStart + 1;
+
+        if (rowStart === 0) {
+          var backRow = rowStart;
+        } else {
+          var backRow = rowStart - 1;
+        }
+
+        if (rowStart === (rows - 1)) {
+          var forwardRow = rowStart;
+        } else {
+          var forwardRow = rowStart + 1;
+        }
+
+        if (columnStart === 0) {
+          var backColumn = columnStart;
+        } else {
+          var backColumn = columnStart - 1;
+        }
+
+        if (columnStart === (columns - 1)) {
+          forwardColumn = columnStart;
+        } else {
+          var forwardColumn = columnStart + 1;
+        }
 
         //The loop below ensures there are no ships currently in the proposed path. If a ship is detected already in a spot in the proposed path, the validHorizontalloop will be broken and validPlacement remains false. The do/while loop will run again with new orientation and placement.
         validHorizontalLoop:
         for (var j = columnStart; j < endColumn; j++) {
           if (board[rowStart][j] === "O" &&
-              (board[rowStart][backColumn] === "O" || board[rowStart][backColumn] == null) &&
-              (board[rowStart][endColumn] === "O" || board[rowStart][endColumn] == null) &&
-              (board[backRow][j] === "O" || board[backRow][j] == null) &&
-              (board[forwardRow][j] === "O" || board[forwardRow][j] == null)) {
+              board[rowStart][backColumn] === "O"  &&
+              board[rowStart][endColumn] === "O" &&
+              board[backRow][j] === "O" &&
+              board[forwardRow][j] === "O") {
             validPlacement = true; //current spot IS NOT occupied
           } else {
             validPlacement = false; //current spot IS occupied
@@ -97,18 +117,38 @@ function addShip(ship, shipLength) {
 
         var endRow = rowStart + shipLength;
         var endColumn = columnStart + shipLength;
-        var backRow = rowStart - 1;
-        var forwardRow = rowStart + 1;
-        var backColumn = columnStart - 1;
-        var forwardColumn = columnStart + 1;
+
+        if (rowStart === 0) {
+          var backRow = rowStart;
+        } else {
+          var backRow = rowStart - 1;
+        }
+
+        if (rowStart === (rows - 1)) {
+          var forwardRow = rowStart;
+        } else {
+          var forwardRow = rowStart + 1;
+        }
+
+        if (columnStart === 0) {
+          var backColumn = columnStart;
+        } else {
+          var backColumn = columnStart - 1;
+        }
+
+        if (columnStart === (columns - 1)) {
+          forwardColumn = columnStart;
+        } else {
+          var forwardColumn = columnStart + 1;
+        }
 
         validVerticalLoop:
         for (var i = rowStart; i < endRow; i++) {
           if (board[i][columnStart] === "O"  &&
-          (board[backRow][columnStart] === "O" || board[backRow][columnStart] == null) &&
-          (board[(endRow)][columnStart] === "O" || board[(endRow)][columnStart] == null) &&
-          (board[i][backColumn] === "O" || board[i][backColumn] == null) &&
-          (board[i][forwardColumn] === "O" || board[i][forwardColumn] == null)) {
+              board[backRow][columnStart] === "O" &&
+              board[(endRow)][columnStart] === "O" &&
+              board[i][backColumn] === "O" &&
+              board[i][forwardColumn] === "O") {
             validPlacement = true; //current spot IS NOT occupied
           } else {
             validPlacement = false; //current spot IS occupied
@@ -127,16 +167,16 @@ function addShip(ship, shipLength) {
   }
 }
 
-function checkBoard(position) {
-  var rowNum = parseInt(position.slice(0,1));
-  var colNum = parseInt(position.slice(1,2));
-  for (var i = 0; i < board.length; i++) {
-    for (var j = 0; j < board[i].length; j++) {
-      if (board[rowNum][colNum] === ship) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-}
+// function checkBoard(position) {
+//   var rowNum = parseInt(position.slice(0,1));
+//   var colNum = parseInt(position.slice(1,2));
+//   for (var i = 0; i < board.length; i++) {
+//     for (var j = 0; j < board[i].length; j++) {
+//       if (board[rowNum][colNum] === ship) {
+//         return true;
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+// }
